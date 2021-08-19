06cdd2b629d69874ea6b208ba894fa25fa1c28e5
Rubbonn <gujojo@hotmail.it>
2021-08-19 13:07:44 +0200

    - Integrato controllo del robot nel file principale

--- app.py
+++ app.py
@@ -1,24 +1,34 @@
 from flask import Flask, render_template, request
-from threading import Thread
-import robot
-import socket
+import gpiozero
 
-# Setup thread per controllo robot
-threadRobot = Thread(target=robot.start)
-threadRobot.start()
+# Setup robot
+robot = gpiozero.Robot((17,4), (22,27), pwm=False)
+robot.stop()
+pwma = gpiozero.PWMOutputDevice(2, frequency=25000, initial_value=0.5)
+pwmb = gpiozero.PWMOutputDevice(3, frequency=25000, initial_value=0.5)
 
+# Setup flask
 app = Flask(__name__)
 
+# Homepage con comandi
 @app.route("/")
 def index():
 	return render_template("index.html")
+
+# Impostazione del movimento del robot
 @app.route("/set-direzione")
 def set_direzione():
-	direzione = request.args.get('direzione', 0, type=str)
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect(('localhost', robot.PORTA))
-	s.sendall(direzione.encode())
-	s.close()
+	direzione = request.args.get('direzione', 0, type=int)
+	if(direzione == 0):
+		robot.stop()
+	elif(direzione == 1):
+		robot.forward()
+	elif(direzione == 2):
+		robot.backward()
+	elif(direzione == 3):
+		robot.right()
+	elif(direzione == 4):
+		robot.left()
 	return ('', 204)
 
 if(__name__ == '__main__'):
